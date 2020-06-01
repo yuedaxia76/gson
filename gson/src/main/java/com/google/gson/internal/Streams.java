baf81e226be4c17781053cea3c40339503f521c8
lenovo <lenovo@LAPTOP-3F8KC97O>
2020-06-01 16:54:47 +0800

    合并 Marcono1234修改

--- gson/src/main/java/com/google/gson/internal/Streams.java
+++ gson/src/main/java/com/google/gson/internal/Streams.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.gson.internal;
 
 import com.google.gson.JsonElement;
@@ -33,88 +32,140 @@ import java.io.Writer;
  * Reads and writes GSON parse trees over streams.
  */
 public final class Streams {
-  private Streams() {
-    throw new UnsupportedOperationException();
-  }
 
-  /**
-   * Takes a reader in any state and returns the next value as a JsonElement.
-   */
-  public static JsonElement parse(JsonReader reader) throws JsonParseException {
-    boolean isEmpty = true;
-    try {
-      reader.peek();
-      isEmpty = false;
-      return TypeAdapters.JSON_ELEMENT.read(reader);
-    } catch (EOFException e) {
-      /*
-       * For compatibility with JSON 1.5 and earlier, we return a JsonNull for
-       * empty documents instead of throwing.
-       */
-      if (isEmpty) {
-        return JsonNull.INSTANCE;
-      }
-      // The stream ended prematurely so it is likely a syntax error.
-      throw new JsonSyntaxException(e);
-    } catch (MalformedJsonException e) {
-      throw new JsonSyntaxException(e);
-    } catch (IOException e) {
-      throw new JsonIOException(e);
-    } catch (NumberFormatException e) {
-      throw new JsonSyntaxException(e);
+    private Streams() {
+        throw new UnsupportedOperationException();
     }
-  }
-
-  /**
-   * Writes the JSON element to the writer, recursively.
-   */
-  public static void write(JsonElement element, JsonWriter writer) throws IOException {
-    TypeAdapters.JSON_ELEMENT.write(writer, element);
-  }
-
-  public static Writer writerForAppendable(Appendable appendable) {
-    return appendable instanceof Writer ? (Writer) appendable : new AppendableWriter(appendable);
-  }
-
-  /**
-   * Adapts an {@link Appendable} so it can be passed anywhere a {@link Writer}
-   * is used.
-   */
-  private static final class AppendableWriter extends Writer {
-    private final Appendable appendable;
-    private final CurrentWrite currentWrite = new CurrentWrite();
-
-    AppendableWriter(Appendable appendable) {
-      this.appendable = appendable;
-    }
-
-    @Override public void write(char[] chars, int offset, int length) throws IOException {
-      currentWrite.chars = chars;
-      appendable.append(currentWrite, offset, offset + length);
-    }
-
-    @Override public void write(int i) throws IOException {
-      appendable.append((char) i);
-    }
-
-    @Override public void flush() {}
-    @Override public void close() {}
 
     /**
-     * A mutable char sequence pointing at a single char[].
+     * Takes a reader in any state and returns the next value as a JsonElement.
      */
-    static class CurrentWrite implements CharSequence {
-      char[] chars;
-      public int length() {
-        return chars.length;
-      }
-      public char charAt(int i) {
-        return chars[i];
-      }
-      public CharSequence subSequence(int start, int end) {
-        return new String(chars, start, end - start);
-      }
+    public static JsonElement parse(JsonReader reader) throws JsonParseException {
+        boolean isEmpty = true;
+        try {
+            reader.peek();
+            isEmpty = false;
+            return TypeAdapters.JSON_ELEMENT.read(reader);
+        } catch (EOFException e) {
+            /*
+       * For compatibility with JSON 1.5 and earlier, we return a JsonNull for
+       * empty documents instead of throwing.
+             */
+            if (isEmpty) {
+                return JsonNull.INSTANCE;
+            }
+            // The stream ended prematurely so it is likely a syntax error.
+            throw new JsonSyntaxException(e);
+        } catch (MalformedJsonException e) {
+            throw new JsonSyntaxException(e);
+        } catch (IOException e) {
+            throw new JsonIOException(e);
+        } catch (NumberFormatException e) {
+            throw new JsonSyntaxException(e);
+        }
     }
-  }
+
+    /**
+     * Writes the JSON element to the writer, recursively.
+     */
+    public static void write(JsonElement element, JsonWriter writer) throws IOException {
+        TypeAdapters.JSON_ELEMENT.write(writer, element);
+    }
+
+    public static Writer writerForAppendable(Appendable appendable) {
+        return appendable instanceof Writer ? (Writer) appendable : new AppendableWriter(appendable);
+    }
+
+    /**
+     * Adapts an {@link Appendable} so it can be passed anywhere a
+     * {@link Writer} is used.
+     */
+    private static final class AppendableWriter extends Writer {
+
+        private final Appendable appendable;
+        private final CurrentWrite currentWrite = new CurrentWrite();
+
+        AppendableWriter(Appendable appendable) {
+            this.appendable = appendable;
+        }
+
+        @Override
+        public void write(char[] chars, int offset, int length) throws IOException {
+            currentWrite.setChars(chars);
+            appendable.append(currentWrite, offset, offset + length);
+        }
+
+        @Override
+        public void flush() {
+        }
+
+        @Override
+        public void close() {
+        }
+
+        // Override these methods for better performance
+        // They would otherwise unnecessarily create Strings or char arrays
+        @Override
+        public void write(int i) throws IOException {
+            appendable.append((char) i);
+        }
+
+        @Override
+        public void write(String str, int off, int len) throws IOException {
+            // Appendable.append turns null -> "null", which is not desired here
+            $Gson$Preconditions.checkNotNull(str);
+            appendable.append(str, off, off + len);
+        }
+
+        @Override
+        public Writer append(CharSequence csq) throws IOException {
+            appendable.append(csq);
+            return this;
+        }
+
+        @Override
+        public Writer append(CharSequence csq, int start, int end) throws IOException {
+            appendable.append(csq, start, end);
+            return this;
+        }
+
+        /**
+         * A mutable char sequence pointing at a single char[].
+         */
+        private static class CurrentWrite implements CharSequence {
+
+            private char[] chars;
+            private String cachedString;
+
+            public void setChars(char[] chars) {
+                this.chars = chars;
+                this.cachedString = null;
+            }
+
+            @Override
+            public int length() {
+                return chars.length;
+            }
+
+            @Override
+            public char charAt(int i) {
+                return chars[i];
+            }
+
+            @Override
+            public CharSequence subSequence(int start, int end) {
+                return new String(chars, start, end - start);
+            }
+
+            // Must return string representation to satisfy toString() contract
+            @Override
+            public String toString() {
+                if (cachedString == null) {
+                    cachedString = new String(chars);
+                }
+                return cachedString;
+            }
+        }
+    }
 
 }
--- gson/src/test/java/com/google/gson/GsonTypeAdapterTest.java
+++ gson/src/test/java/com/google/gson/GsonTypeAdapterTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.gson;
 
 import java.lang.reflect.Type;
@@ -23,126 +22,143 @@ import java.util.concurrent.atomic.AtomicLong;
 import junit.framework.TestCase;
 
 /**
- * Contains numerous tests involving registered type converters with a Gson instance.
+ * Contains numerous tests involving registered type converters with a Gson
+ * instance.
  *
  * @author Inderjeet Singh
  * @author Joel Leitch
  */
 public class GsonTypeAdapterTest extends TestCase {
-  private Gson gson;
 
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    gson = new GsonBuilder()
-        .registerTypeAdapter(AtomicLong.class, new ExceptionTypeAdapter())
-        .registerTypeAdapter(AtomicInteger.class, new AtomicIntegerTypeAdapter())
-        .create();
-  }
+    private Gson gson;
 
-  public void testDefaultTypeAdapterThrowsParseException() throws Exception {
-    try {
-      gson.fromJson("{\"abc\":123}", BigInteger.class);
-      fail("Should have thrown a JsonParseException");
-    } catch (JsonParseException expected) { }
-  }
-
-  public void testTypeAdapterThrowsException() throws Exception {
-    try {
-      gson.toJson(new AtomicLong(0));
-      fail("Type Adapter should have thrown an exception");
-    } catch (IllegalStateException expected) { }
-
-    try {
-      gson.fromJson("123", AtomicLong.class);
-      fail("Type Adapter should have thrown an exception");
-    } catch (JsonParseException expected) { }
-  }
-
-  public void testTypeAdapterProperlyConvertsTypes() throws Exception {
-    int intialValue = 1;
-    AtomicInteger atomicInt = new AtomicInteger(intialValue);
-    String json = gson.toJson(atomicInt);
-    assertEquals(intialValue + 1, Integer.parseInt(json));
-
-    atomicInt = gson.fromJson(json, AtomicInteger.class);
-    assertEquals(intialValue, atomicInt.get());
-  }
-
-  public void testTypeAdapterDoesNotAffectNonAdaptedTypes() throws Exception {
-    String expected = "blah";
-    String actual = gson.toJson(expected);
-    assertEquals("\"" + expected + "\"", actual);
-
-    actual = gson.fromJson(actual, String.class);
-    assertEquals(expected, actual);
-  }
-
-  private static class ExceptionTypeAdapter
-      implements JsonSerializer<AtomicLong>, JsonDeserializer<AtomicLong> {
-    @Override public JsonElement serialize(
-        AtomicLong src, Type typeOfSrc, JsonSerializationContext context) {
-      throw new IllegalStateException();
-    }
-    @Override public AtomicLong deserialize(
-        JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
-      throw new IllegalStateException();
-    }
-  }
-
-  private static class AtomicIntegerTypeAdapter
-      implements JsonSerializer<AtomicInteger>, JsonDeserializer<AtomicInteger> {
-    @Override public JsonElement serialize(AtomicInteger src, Type typeOfSrc, JsonSerializationContext context) {
-      return new JsonPrimitive(src.incrementAndGet());
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        gson = new GsonBuilder()
+                .registerTypeAdapter(AtomicLong.class, new ExceptionTypeAdapter())
+                .registerTypeAdapter(AtomicInteger.class, new AtomicIntegerTypeAdapter())
+                .create();
     }
 
-    @Override public AtomicInteger deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
-      int intValue = json.getAsInt();
-      return new AtomicInteger(--intValue);
+    public void testDefaultTypeAdapterThrowsParseException() throws Exception {
+        try {
+            gson.fromJson("{\"abc\":123}", BigInteger.class);
+            fail("Should have thrown a JsonParseException");
+        } catch (JsonParseException expected) {
+        }
     }
-  }
 
-  static abstract class Abstract {
-    String a;
-  }
-
-  static class Concrete extends Abstract {
-    String b;
-  }
-
-  // https://groups.google.com/d/topic/google-gson/EBmOCa8kJPE/discussion
-  public void testDeserializerForAbstractClass() {
-    Concrete instance = new Concrete();
-    instance.a = "android";
-    instance.b = "beep";
-    assertSerialized("{\"a\":\"android\"}", Abstract.class, true, true, instance);
-    assertSerialized("{\"a\":\"android\"}", Abstract.class, true, false, instance);
-    assertSerialized("{\"a\":\"android\"}", Abstract.class, false, true, instance);
-    assertSerialized("{\"a\":\"android\"}", Abstract.class, false, false, instance);
-    assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, true, true, instance);
-    assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, true, false, instance);
-    assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, false, true, instance);
-    assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, false, false, instance);
-  }
-
-  private void assertSerialized(String expected, Class<?> instanceType, boolean registerAbstractDeserializer,
-      boolean registerAbstractHierarchyDeserializer, Object instance) {
-    JsonDeserializer<Abstract> deserializer = new JsonDeserializer<Abstract>() {
-      public Abstract deserialize(JsonElement json, Type typeOfT,
-          JsonDeserializationContext context) throws JsonParseException {
-        throw new AssertionError();
-      }
-    };
-    GsonBuilder builder = new GsonBuilder();
-    if (registerAbstractDeserializer) {
-      builder.registerTypeAdapter(Abstract.class, deserializer);
+    public void testTypeAdapterThrowsException() throws Exception {
+        try {
+            gson.toJson(new AtomicLong(0));
+            fail("Type Adapter should have thrown an exception");
+        } catch (IllegalStateException expected) {
+        }
+        // Verify that serializer is made null-safe, i.e. it is not called for null
+        assertEquals("null", gson.toJson(null, AtomicLong.class));
+        try {
+            gson.fromJson("123", AtomicLong.class);
+            fail("Type Adapter should have thrown an exception");
+        } catch (JsonParseException expected) {
+        }
+        // Verify that deserializer is made null-safe, i.e. it is not called for null
+        assertNull(gson.fromJson(JsonNull.INSTANCE, AtomicLong.class));
     }
-    if (registerAbstractHierarchyDeserializer) {
-      builder.registerTypeHierarchyAdapter(Abstract.class, deserializer);
+
+    public void testTypeAdapterProperlyConvertsTypes() throws Exception {
+        int intialValue = 1;
+        AtomicInteger atomicInt = new AtomicInteger(intialValue);
+        String json = gson.toJson(atomicInt);
+        assertEquals(intialValue + 1, Integer.parseInt(json));
+
+        atomicInt = gson.fromJson(json, AtomicInteger.class);
+        assertEquals(intialValue, atomicInt.get());
     }
-    Gson gson = builder.create();
-    assertEquals(expected, gson.toJson(instance, instanceType));
-  }
+
+    public void testTypeAdapterDoesNotAffectNonAdaptedTypes() throws Exception {
+        String expected = "blah";
+        String actual = gson.toJson(expected);
+        assertEquals("\"" + expected + "\"", actual);
+
+        actual = gson.fromJson(actual, String.class);
+        assertEquals(expected, actual);
+    }
+
+    private static class ExceptionTypeAdapter
+            implements JsonSerializer<AtomicLong>, JsonDeserializer<AtomicLong> {
+
+        @Override
+        public JsonElement serialize(
+                AtomicLong src, Type typeOfSrc, JsonSerializationContext context) {
+            throw new IllegalStateException();
+        }
+
+        @Override
+        public AtomicLong deserialize(
+                JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            throw new IllegalStateException();
+        }
+    }
+
+    private static class AtomicIntegerTypeAdapter
+            implements JsonSerializer<AtomicInteger>, JsonDeserializer<AtomicInteger> {
+
+        @Override
+        public JsonElement serialize(AtomicInteger src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.incrementAndGet());
+        }
+
+        @Override
+        public AtomicInteger deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            int intValue = json.getAsInt();
+            return new AtomicInteger(--intValue);
+        }
+    }
+
+    static abstract class Abstract {
+
+        String a;
+    }
+
+    static class Concrete extends Abstract {
+
+        String b;
+    }
+
+    // https://groups.google.com/d/topic/google-gson/EBmOCa8kJPE/discussion
+    public void testDeserializerForAbstractClass() {
+        Concrete instance = new Concrete();
+        instance.a = "android";
+        instance.b = "beep";
+        assertSerialized("{\"a\":\"android\"}", Abstract.class, true, true, instance);
+        assertSerialized("{\"a\":\"android\"}", Abstract.class, true, false, instance);
+        assertSerialized("{\"a\":\"android\"}", Abstract.class, false, true, instance);
+        assertSerialized("{\"a\":\"android\"}", Abstract.class, false, false, instance);
+        assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, true, true, instance);
+        assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, true, false, instance);
+        assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, false, true, instance);
+        assertSerialized("{\"b\":\"beep\",\"a\":\"android\"}", Concrete.class, false, false, instance);
+    }
+
+    private void assertSerialized(String expected, Class<?> instanceType, boolean registerAbstractDeserializer,
+            boolean registerAbstractHierarchyDeserializer, Object instance) {
+        JsonDeserializer<Abstract> deserializer = new JsonDeserializer<Abstract>() {
+            public Abstract deserialize(JsonElement json, Type typeOfT,
+                    JsonDeserializationContext context) throws JsonParseException {
+                throw new AssertionError();
+            }
+        };
+        GsonBuilder builder = new GsonBuilder();
+        if (registerAbstractDeserializer) {
+            builder.registerTypeAdapter(Abstract.class, deserializer);
+        }
+        if (registerAbstractHierarchyDeserializer) {
+            builder.registerTypeHierarchyAdapter(Abstract.class, deserializer);
+        }
+        Gson gson = builder.create();
+        assertEquals(expected, gson.toJson(instance, instanceType));
+    }
 }
